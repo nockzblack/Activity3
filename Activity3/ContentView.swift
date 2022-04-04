43e752ec53d144cebf8b286e7799837e154330df
Fernando Benavides <nockzblack@gmail.com>
2022-04-04 13:25:32 -0500

    Header View with user photo added

--- Activity3/ContentView.swift
+++ Activity3/ContentView.swift
@@ -8,11 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
+                .edgesIgnoringSafeArea(.all)
+            
+            Header()
+        }
+        
+        
+              
     }
 }
+
+// MARK: Header
+
+
+struct Header: View {
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text("Good Morning, ")
+                    .font(.system(size: 19, weight: .medium, design: .serif))
+                    .foregroundColor(Color(#colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)))
+                                     
+                Text("User")
+                    .font(.system(size: 28, weight: .bold, design: .serif))
+                    .foregroundColor(Color(#colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)))
+                    .padding(.top, 5)
+                                     
+            }
+            .padding(.leading, 30)
+            Spacer()
+            Image("loggeduser")
+                .resizable()
+                .frame(width: 50, height: 50)
+                .padding(.trailing, 30)
+            
+        }
+    }
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
