c9321c82e51705c66101de17b95ea2c9087235b1
Fernando Benavides <nockzblack@gmail.com>
2022-04-04 13:06:30 -0500

    Initial Commit

---
+++ Activity3/Activity3App.swift
@@ -0,0 +1,17 @@
+//
+//  Activity3App.swift
+//  Activity3
+//
+//  Created by Fernando's Mac on 04/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Activity3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
